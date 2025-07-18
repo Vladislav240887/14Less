a7a86d9390ed5ecd24f9f9f3f1eb8ff30a34ba4c
Vlad <vladislavsendzuk567@gmail.com>
2025-07-18 20:25:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// 2 завдання
+// const list = document.getElementById('list');
+// const button = document.getElementById('button');
+//
+// button.addEventListener('click', () => {
+//     const newItem = document.createElement('li');
+//
+//     newItem.textContent = `Елемент ${list.children.length + 1}`;
+//     list.appendChild(newItem)
+// })
+
+// 5 завдання
+// function sortArray(arr) {
+//
+//     return [...arr].sort((a, b) => a - b);
+// }
+// const arr = [3, 2, 6, 1, 8];
+// const sortedArray = sortArray(arr);
+//
+// console.log(sortedArray);
